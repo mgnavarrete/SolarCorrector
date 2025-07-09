5bf2a8c607e4399e1690a24e75b6121fdeb89303
mgnavarrete <mgnavarrete@uc.cl>
2025-07-09 11:30:52 -0400

    main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     folders_list = FileManager().select_directories()
     
     for folder in folders_list:
-        solarCorrection = SolarCorrection(folder)
-        solarCorrection.findFlights()
-        solarCorrection.get_seg_paneles()
+        SC = SolarCorrector(folder)
+        SC.findFlights(3)
+        SC.get_seg_paneles(save_masks=True)
     
