39b750226564ea6dfda325d2adb22e6ec758b113
mgnavarrete <mgnavarrete@uc.cl>
2025-07-09 16:14:07 -0400

    geo

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     
     for folder in folders_list:
         SC = SolarCorrector(folder)
-        #SC.reset_metadata(var='all')    
-        #SC.save_geo_matrix()
+        SC.reset_metadata(var='all')    
+        SC.save_geo_matrix()
         SC.findFlights(4, save_kml=False)
         SC.get_seg_paneles(save_masks=True)
         SC.correct_E()
